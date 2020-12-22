343d9026c3a87668b64714cd68433afde0a5443b
Rafał Frączek <rafffraczek98@gmail.com>
2020-12-22 11:42:43 +0100

    Adding base page template, another page inherit from them

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author':'RafalFr',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'August 27, 2018'
+    },
+{
+        'author': 'MichalFr',
+        'title': 'Blog Post 2',
+        'content': 'Second  Post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(requset):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(requset, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #myapp
+    'blog.apps.BlogConfig',
+    #django app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

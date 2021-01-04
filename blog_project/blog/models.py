d9aed6a89e51dd78382df2cc6941a7c4197f8457
Rafał Frączek <rafffraczek98@gmail.com>
2021-01-04 15:13:42 +0100

    database migration, adding blog edditing in admin page

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#class in database, own table in data base
+class Post(models.Model):
+    #field in table
+    title = models.CharField(max_length =100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if user if delete, delete their post to
+
+    def __str__(self):
+        return self.title
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 # Create your views here.
 
-posts = [
-    {
-        'author':'RafalFr',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 27, 2018'
-    },
-{
-        'author': 'MichalFr',
-        'title': 'Blog Post 2',
-        'content': 'Second  Post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(requset):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(requset, 'blog/home.html', context)
 
